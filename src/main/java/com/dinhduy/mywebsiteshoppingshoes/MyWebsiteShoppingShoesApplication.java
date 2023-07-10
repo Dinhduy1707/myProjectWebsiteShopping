0be3a88fda5c140b9d8f67ead497319795172cd7
OFFICEVNPAY\duynd <duynd@vnpay.vn>
2023-07-10 23:00:17 +0700

    update code

---
+++ src/main/java/com/dinhduy/mywebsiteshoppingshoes/MyWebsiteShoppingShoesApplication.java
@@ -0,0 +1,13 @@
+package com.dinhduy.mywebsiteshoppingshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebsiteShoppingShoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebsiteShoppingShoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dinhduy/mywebsiteshoppingshoes/MyWebsiteShoppingShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinhduy.mywebsiteshoppingshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteShoppingShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
